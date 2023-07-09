79e6880fb4ac0633c2bc9e201f28b4acf9c7afe4
phapdev <luongphap1810@gmail.com>
2023-07-09 22:16:40 +0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
 fn main() {
-    assert_eq!(count_char_occurrences("hello", 'l'), 2);
-}
-
-fn count_char_occurrences(string: &str, ch: char) -> i32 {
-   let num:i32 = (string.chars().filter(|&c| c == ch).count()) as i32;
-   println!("{num}");
-   num
+    println!("Hello world!");
 }
