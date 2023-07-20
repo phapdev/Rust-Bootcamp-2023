93cb6756be237d5ac8bb54f2ea6dae564a61296d
phapdev <luongphap1810@gmail.com>
2023-07-21 01:51:02 +0700

    class-5

--- exercises/error-handling/src/lib.rs
+++ exercises/error-handling/src/lib.rs
@@ -2,12 +2,12 @@
 // Make it compile in unit test
 // Run tests
 // Hint: Convert Option to Result
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        None
+        Err("`name` was empty; it must be nonempty.".to_string())
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
 // Exercise 2
@@ -17,7 +17,7 @@ fn generate_nametag_text(name: String) -> Option<String> {
 use std::num::ParseIntError;
 
 fn parse_number(s: &str) -> Result<i32, ParseIntError> {
-    todo!()
+    s.parse()
 }
 
 // Exercise 3
@@ -36,14 +36,26 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value < 0 {
+            Err(CreationError::Negative)
+        } else if value == 0 {
+            Err(CreationError::Zero)
+        }else {
+            Ok(PositiveNonzeroInteger(value as u64))
+        }
+        // match value {
+        //     x if x < 0 => Err(CreationError::Negative),
+        //     x if x == 0 => Err(CreationError::Zero),
+        //     x => Ok(PositiveNonzeroInteger(x as u64)),
+        // }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    // Result : OK & Errr
+    // Options: Some & None
     /// Test for exercise 1
     #[test]
     fn exercise1_should_work() {
@@ -64,7 +76,7 @@ mod tests {
     fn exercise2_should_work() {
         assert_eq!(parse_number("42"), Ok(42));
         assert_eq!(
-            parse_number("invalid"),
+            parse_number("invalid").map_err(|e| e.to_string()),
             Err("invalid digit found in string".parse().unwrap())
         );
     }
